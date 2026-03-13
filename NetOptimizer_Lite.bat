bb6b434f0a88bff51237c4ccf173ef3488d452b9
alisakkaf <alisakkaf010@gmail.com>
2026-03-13 23:14:48 +0300

    ⚙️ System Compatibility Update v2.4: Introduced Lite Version for legacy Windows 7/8 support (stripped ANSI and UTF-8 dependency).

---
+++ NetOptimizer_Lite.bat
@@ -0,0 +1,279 @@
+@echo off
+setlocal EnableDelayedExpansion
+title NetOptimizer Lite v2.4 - By ALI SAKKAF
+
+:: ==========================================
+:: ADMINISTRATOR PRIVILEGES CHECK
+:: ==========================================
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo.
+    echo [!] ERROR: This script requires Administrator privileges.
+    echo [*] Requesting elevation... please wait.
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /b
+)
+
+:MENU
+cls
+echo.
+echo ==============================================================================
+echo                      ULTIMATE NETWORK ^& UPDATE KILLER (LITE)
+echo                      Developed By: ALI SAKKAF
+echo                      GitHub: github.com/alisakkaf
+echo ==============================================================================
+echo.
+echo [ SYSTEM NETWORK CONTROL ]
+echo ------------------------------------------------------------------------------
+echo   [1] Disable ALL Network-Hungry Services (Max Performance)
+echo   [2] Restore ALL Windows Services to Default State
+echo   [3] Enable Windows Update ^& Phone Link ONLY
+echo.
+echo [ BROWSERS AUTO-UPDATE CONTROL ]
+echo ------------------------------------------------------------------------------
+echo   [4] Temporary Pause: Kill active updaters (Restores on reboot)
+echo   [5] Permanent Block: Hard Kill ^& Disable Services/GPO
+echo   [6] Restore Defaults: Enable all browser updates
+echo.
+echo   [7] Exit Application
+echo.
+set /p choice=" >> Select an option [1-7]: "
+
+if "%choice%"=="1" goto DISABLE
+if "%choice%"=="2" goto ENABLE
+if "%choice%"=="3" goto ENABLE_WU
+if "%choice%"=="4" goto DISABLE_BROWSERS_TEMP
+if "%choice%"=="5" goto DISABLE_BROWSERS_PERM
+if "%choice%"=="6" goto ENABLE_BROWSERS
+if "%choice%"=="7" exit
+goto MENU
+
+:: ==========================================
+:: [1] DISABLE ALL SERVICES
+:: ==========================================
+:DISABLE
+cls
+echo.
+echo ==============================================================================
+echo [*] DEPLOYING MAXIMUM NETWORK PERFORMANCE PROTOCOL...
+echo ==============================================================================
+echo.
+
+echo [KILL] Terminating Resource-Heavy Background UI ^& Telemetry Processes...
+for %%P in (msedgewebview2.exe OneDrive.exe Widgets.exe CompatTelRunner.exe DeviceCensus.exe software_reporter_tool.exe gamebarpresencewriter.exe) do ( taskkill /F /IM %%P /T >nul 2>&1 )
+echo [+] DONE.
+echo.
+
+echo [INFO] Stopping Windows Update Core Engine...
+for %%S in (wuauserv bits dosvc UsoSvc WaaSMedicSvc) do ( net stop %%S /y >nul 2>&1 )
+echo [+] DONE.
+echo.
+
+echo [INFO] Stopping Telemetry, Sync, Error Reporting ^& App Store Services...
+for %%S in (DiagTrack dmwappushservice PcaSvc CDPSvc WpnService NcbService InstallService MapsBroker lfsvc OneSyncSvc AppXSVC ClipSVC SysMain GamingServices GamingServicesNet XblAuthManager WerSvc WSearch PhoneSvc PushToInstall diagnosticshub.standardcollector.service TrkWks) do ( net stop %%S /y >nul 2>&1 )
+echo [+] DONE.
+echo.
+
+echo [INFO] Permanently Disabling Services from Auto-Start...
+for %%S in (wuauserv bits dosvc UsoSvc DiagTrack dmwappushservice PcaSvc CDPSvc WpnService NcbService InstallService MapsBroker lfsvc OneSyncSvc AppXSVC ClipSVC SysMain GamingServices GamingServicesNet XblAuthManager WerSvc WSearch PhoneSvc PushToInstall diagnosticshub.standardcollector.service TrkWks) do (
+    sc config %%S start= disabled >nul 2>&1
+)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 4 /f >nul 2>&1
+echo [+] DONE.
+echo.
+
+echo [INFO] Suspending Telemetry ^& Scheduled Maintenance Tasks...
+for %%T in ("Application Experience\ProgramDataUpdater" "Customer Experience Improvement Program\Consolidator" "Customer Experience Improvement Program\UsbCeip" "WindowsUpdate\Scheduled Start" "UpdateOrchestrator\Schedule Scan") do (
+    schtasks /Change /TN "Microsoft\Windows\%%~T" /Disable >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [INFO] Enforcing "Metered Connection" on Network Adapters...
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\DefaultMediaCost" /v Ethernet /t REG_DWORD /d 2 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\DefaultMediaCost" /v WiFi /t REG_DWORD /d 2 /f >nul 2>&1
+echo [+] DONE.
+echo.
+
+echo [INFO] Resetting Network Stack ^& Clearing DNS Cache...
+ipconfig /flushdns >nul 2>&1
+netsh winsock reset >nul 2>&1
+netsh int ip reset >nul 2>&1
+echo [+] DONE.
+echo.
+echo [ SUCCESS ] OPERATION COMPLETE: WINDOWS NETWORK CONSUMPTION IS NOW AT 0%%.
+echo.
+pause
+goto MENU
+
+:: ==========================================
+:: [2] ENABLE ALL SERVICES
+:: ==========================================
+:ENABLE
+cls
+echo.
+echo ==============================================================================
+echo [*] RESTORING SYSTEM SERVICES TO DEFAULT CONFIGURATION...
+echo ==============================================================================
+echo.
+
+echo [INFO] Restoring Service Startup Types...
+for %%S in (wuauserv bits dosvc UsoSvc DiagTrack dmwappushservice PcaSvc CDPSvc WpnService NcbService InstallService MapsBroker lfsvc OneSyncSvc AppXSVC ClipSVC SysMain GamingServices GamingServicesNet XblAuthManager WerSvc WSearch PhoneSvc PushToInstall diagnosticshub.standardcollector.service TrkWks) do (
+    sc config %%S start= demand >nul 2>&1
+)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 3 /f >nul 2>&1
+echo [+] DONE.
+echo.
+
+echo [INFO] Re-Enabling Scheduled Maintenance Tasks...
+for %%T in ("Application Experience\ProgramDataUpdater" "WindowsUpdate\Scheduled Start" "UpdateOrchestrator\Schedule Scan") do (
+    schtasks /Change /TN "Microsoft\Windows\%%~T" /Enable >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [INFO] Removing "Metered Connection" Restrictions...
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\DefaultMediaCost" /v Ethernet /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\DefaultMediaCost" /v WiFi /t REG_DWORD /d 1 /f >nul 2>&1
+echo [+] DONE.
+echo.
+echo [ SUCCESS ] OPERATION COMPLETE: WINDOWS IS BACK TO NORMAL.
+echo.
+pause
+goto MENU
+
+:: ==========================================
+:: [3] ENABLE WINDOWS UPDATE ONLY
+:: ==========================================
+:ENABLE_WU
+cls
+echo.
+echo ==============================================================================
+echo [*] ACTIVATING WINDOWS UPDATE ^& PHONE LINK SERVICES...
+echo ==============================================================================
+echo.
+echo [INFO] Re-configuring essential services...
+sc config bits start= demand >nul 2>&1
+sc config wuauserv start= demand >nul 2>&1
+sc config PhoneSvc start= demand >nul 2>&1
+echo [INFO] Starting services...
+net start bits >nul 2>&1
+net start wuauserv >nul 2>&1
+net start PhoneSvc >nul 2>&1
+echo [+] DONE.
+echo.
+echo [ SUCCESS ] YOU CAN NOW CHECK FOR UPDATES AND USE PHONE LINK.
+echo.
+pause
+goto MENU
+
+:: ==========================================
+:: [4] TEMPORARY PAUSE BROWSERS UPDATES
+:: ==========================================
+:DISABLE_BROWSERS_TEMP
+cls
+echo.
+echo ==============================================================================
+echo [*] INITIATING TEMPORARY BROWSER UPDATER SUSPENSION...
+echo ==============================================================================
+echo.
+
+echo [KILL] Terminating Active Updater Processes in RAM...
+for %%P in (GoogleUpdate.exe BraveUpdate.exe MicrosoftEdgeUpdate.exe maintenanceservice.exe opera_autoupdate.exe updater.exe BraveUpdateOnDemand.exe BraveCrashHandler.exe BraveCrashHandler64.exe BraveCrashHandlerArm64.exe BraveUpdateBroker.exe BraveUpdateComRegisterShell64.exe BraveUpdateComRegisterShellArm64.exe BraveUpdateCore.exe remoting_crashpad_handler.exe remoting_native_messaging_host.exe remote_assistance_host_uiaccess.exe remote_open_url.exe remote_assistance_host.exe remote_security_key.exe remoting_start_host.exe remote_webauthn.exe remoting_desktop.exe remoting_host.exe elevated_tracing_service.exe mscopilot.exe elevation_service.exe msedge_pwa_launcher.exe passkey_authenticator_plugin.exe notification_helper.exe notification_click_helper.exe msedge_proxy.exe identity_helper.exe pwahelper.exe ie_to_edge_stub.exe cookie_exporter.exe copilot_setup.exe) do ( taskkill /F /IM %%P /T >nul 2>&1 )
+echo [+] DONE.
+echo.
+
+echo [INFO] Stopping Browser Updater Services (Without disabling them)...
+for %%B in (gupdate gupdatem braveupdate bravemupdate edgeupdate edgeupdatem MozillaMaintenance) do (
+    net stop %%B /y >nul 2>&1
+)
+echo [+] DONE.
+echo.
+echo [ NOTE ] Updaters are stopped for this session but will return on PC restart.
+echo [ SUCCESS ] BROWSERS TEMPORARILY PAUSED.
+echo.
+pause
+goto MENU
+
+:: ==========================================
+:: [5] PERMANENT DISABLE BROWSERS UPDATES
+:: ==========================================
+:DISABLE_BROWSERS_PERM
+cls
+echo.
+echo ==============================================================================
+echo [*] INITIATING PERMANENT BROWSER UPDATER ELIMINATION PROTOCOL...
+echo ==============================================================================
+echo.
+
+echo [KILL] Hunting ^& Terminating Active Updater Processes in RAM...
+for %%P in (GoogleUpdate.exe BraveUpdate.exe MicrosoftEdgeUpdate.exe maintenanceservice.exe opera_autoupdate.exe updater.exe BraveUpdateOnDemand.exe BraveCrashHandler.exe BraveCrashHandler64.exe BraveCrashHandlerArm64.exe BraveUpdateBroker.exe BraveUpdateComRegisterShell64.exe BraveUpdateComRegisterShellArm64.exe BraveUpdateCore.exe remoting_crashpad_handler.exe remoting_native_messaging_host.exe remote_assistance_host_uiaccess.exe remote_open_url.exe remote_assistance_host.exe remote_security_key.exe remoting_start_host.exe remote_webauthn.exe remoting_desktop.exe remoting_host.exe elevated_tracing_service.exe mscopilot.exe elevation_service.exe msedge_pwa_launcher.exe passkey_authenticator_plugin.exe notification_helper.exe notification_click_helper.exe msedge_proxy.exe identity_helper.exe pwahelper.exe ie_to_edge_stub.exe cookie_exporter.exe copilot_setup.exe) do ( taskkill /F /IM %%P /T >nul 2>&1 )
+echo [+] DONE.
+echo.
+
+echo [INFO] Disabling Chrome, Brave, Edge ^& Firefox Updater Services...
+for %%B in (gupdate gupdatem braveupdate bravemupdate edgeupdate edgeupdatem MozillaMaintenance) do (
+    net stop %%B /y >nul 2>&1
+    sc config %%B start= disabled >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [INFO] Eradicating Browser Scheduled Update Tasks...
+for %%T in ("\GoogleUpdateTaskMachineCore" "\GoogleUpdateTaskMachineUA" "\BraveUpdateTaskMachineCore" "\BraveUpdateTaskMachineUA" "\MicrosoftEdgeUpdateTaskMachineCore" "\MicrosoftEdgeUpdateTaskMachineUA") do (
+    schtasks /Change /TN "%%~T" /Disable >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [LOCK] Injecting Local Group Policies (GPO) to Block Internal Updates...
+reg add "HKLM\SOFTWARE\Policies\Google\Update" /v UpdateDefault /t REG_DWORD /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Policies\BraveSoftware\Update" /v UpdateDefault /t REG_DWORD /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v UpdateDefault /t REG_DWORD /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Policies\Mozilla\Firefox" /v DisableAppUpdate /t REG_DWORD /d 1 /f >nul 2>&1
+echo [+] DONE.
+echo.
+echo [ SUCCESS ] BROWSERS WILL NO LONGER UPDATE IN BACKGROUND.
+echo.
+pause
+goto MENU
+
+:: ==========================================
+:: [6] ENABLE BROWSERS UPDATES
+:: ==========================================
+:ENABLE_BROWSERS
+cls
+echo.
+echo ==============================================================================
+echo [*] RESTORING BROWSER AUTO-UPDATE FUNCTIONALITY (TEMP ^& PERM)...
+echo ==============================================================================
+echo.
+
+echo [INFO] Re-Enabling Browser Updater Services...
+for %%B in (gupdate gupdatem braveupdate bravemupdate edgeupdate edgeupdatem MozillaMaintenance) do (
+    sc config %%B start= demand >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [INFO] Re-Enabling Browser Scheduled Tasks...
+for %%T in ("\GoogleUpdateTaskMachineCore" "\GoogleUpdateTaskMachineUA" "\BraveUpdateTaskMachineCore" "\BraveUpdateTaskMachineUA" "\MicrosoftEdgeUpdateTaskMachineCore" "\MicrosoftEdgeUpdateTaskMachineUA") do (
+    schtasks /Change /TN "%%~T" /Enable >nul 2>&1
+)
+echo [+] DONE.
+echo.
+
+echo [UNLOCK] Removing Update Blocks from Local Group Policies (GPO)...
+reg delete "HKLM\SOFTWARE\Policies\Google\Update" /v UpdateDefault /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Policies\BraveSoftware\Update" /v UpdateDefault /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v UpdateDefault /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Policies\Mozilla\Firefox" /v DisableAppUpdate /f >nul 2>&1
+echo [+] DONE.
+echo.
+echo [ SUCCESS ] BROWSERS CAN NOW UPDATE NORMALLY.
+echo.
+pause
+goto MENU
